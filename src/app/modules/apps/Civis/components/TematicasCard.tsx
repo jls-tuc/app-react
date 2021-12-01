44f6cb640477f74077ed3a93cbf81aa5721b1c1f
Jose Luis <65769995+jls-tuc@users.noreply.github.com>
2021-12-01 15:04:15 -0300

    fixcivis

--- src/app/modules/apps/Civis/components/TematicasCard.tsx
+++ src/app/modules/apps/Civis/components/TematicasCard.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import {useDispatch} from 'react-redux'
+import React, {useEffect, useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {ISTATE} from '../../../../../setup/redux/interface/reduxState.interface'
 
 import {tematicaAction} from '../Redux/TematicaAction'
 
@@ -7,45 +8,39 @@ type Props = {
   className: string
 }
 interface tamaticaState {
-  id: number
-  tenaticaName: string
-  tematicaColor: string
-  servActivos: string
-  buttonColor: string
+  id: string
+  tipo_detalle: string
+  detalle_modulo: string
+  acceso_nombre: string
+  detalle_html: {
+    buttonColor: string
+    accesoColor: string
+  }
+  web_service: [
+    {
+      organismo_nombre: string
+      nombre_ws: string
+      activo: boolean
+      fecha_alta: string
+      fecha_baja: string
+    }
+  ]
 }
-const tematicas = [
-  {
-    id: 1,
-    tenaticaName: 'Salud',
-    tematicaColor: 'bg-success',
-    servActivos: '1',
-    buttonColor: 'badge-light-success',
-  },
-  {
-    id: 2,
-    tenaticaName: 'Violencia',
-    tematicaColor: 'bg-danger',
-    servActivos: '3',
-    buttonColor: 'badge-light-danger',
-  },
-  {
-    id: 3,
-    tenaticaName: 'Educación',
-    tematicaColor: 'bg-primary',
-    servActivos: '1',
-    buttonColor: 'badge-light-primary',
-  },
-  {
-    id: 4,
-    tenaticaName: 'Accesibilidad',
-    tematicaColor: 'bg-warning',
-    servActivos: '1',
-    buttonColor: 'badge-light-warning',
-  },
-]
 
 const TematicasCard: React.FC<Props> = ({className}) => {
   const dispatch = useDispatch()
+  const [tematicaPerfil, setTematicaPerfil] = useState([])
+  const dashState: any = useSelector((state: ISTATE) => state.dashBoard)
+  const cargarPerfil = () => {
+    let {modulos} = dashState
+    let modulo = modulos.filter((ele: any) => ele.nombre === 'civis')
+    setTematicaPerfil(modulo[0].WSmodulo)
+  }
+  useEffect(() => {
+    cargarPerfil()
+    return () => {}
+  }, [])
+
   const handleChange = (tematica: tamaticaState) => {
     dispatch(tematicaAction(tematica))
   }
@@ -61,10 +56,12 @@ const TematicasCard: React.FC<Props> = ({className}) => {
 
       <div className='card-body pt-2'>
         {/* begin::Item */}
-        {tematicas.map((item) => (
+        {tematicaPerfil.map((item: tamaticaState) => (
           <div className='d-flex align-items-center mb-8' key={item.id}>
             {/* begin::Bullet */}
-            <span className={`bullet bullet-vertical h-40px ${item.tematicaColor}`}></span>
+            <span
+              className={`bullet bullet-vertical h-40px ${item.detalle_html.accesoColor}`}
+            ></span>
             {/* end::Bullet */}
             {/* begin::Checkbox */}
             <div className='form-check form-check-custom form-check-solid mx-5'>
@@ -72,7 +69,7 @@ const TematicasCard: React.FC<Props> = ({className}) => {
                 className='form-check-input'
                 type='checkbox'
                 name='tematica'
-                value={item.tenaticaName}
+                value={item.acceso_nombre}
                 onChange={() => handleChange(item)}
               />
             </div>
@@ -80,13 +77,13 @@ const TematicasCard: React.FC<Props> = ({className}) => {
             {/* begin::Description */}
             <div className='flex-grow-1'>
               <a href='#' className='text-gray-800 text-hover-primary fw-bolder fs-6'>
-                {item.tenaticaName}
+                {item.acceso_nombre}
               </a>
-              <span className='text-muted fw-bold d-block'>{`Servicios Activos ${item.servActivos}`}</span>
+              <span className='text-muted fw-bold d-block'>{`Servicios Activos ${item.web_service.length}`}</span>
             </div>
             {/* end::Description */}
 
-            <span className={`badge ${item.buttonColor} fs-8 fw-bolder`}>Info</span>
+            <span className={`badge ${item.detalle_html.buttonColor} fs-8 fw-bolder`}>Info</span>
           </div>
         ))}
 
